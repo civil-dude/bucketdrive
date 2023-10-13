dbf05dfb61acacee0333ebceae42ef9309c8630d
xadhrit <maharajrabel@gmail.com>
2023-10-13 15:39:26 +0530

    arxiv

--- app/src/pages/AddPage.tsx
+++ app/src/pages/AddPage.tsx
@@ -61,7 +61,7 @@ const AddPage = () => {
         <input
           type="url"
           pattern="https?://.+\.edu/.+|https?://arxiv.org/.+|https?://.+\.ac\.uk/.+|https?://.+\.edu\.au/.+|https?://.+\.gov/.+|https?://.+\.arxiv\.org/.+|https?://.+\.biorxiv\.org/.+"
-          placeholder="Enter research paper link"
+          placeholder="Official Research Paper Url"
           value={addlink}
           onChange={(e) => setAddLink(e.target.value)}
           className="flex-grow border-2 p-2 rounded-md"
